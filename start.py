0752e5a0de6d263ff7a11f96ceac097e8169da20
schahin0001 <schahinheidari@gmail.com>
2023-03-03 11:52:15 +0100

    update advance and standard ui and also added advance class and added negative btn, negative function

--- start.py
+++ start.py
@@ -1,36 +1,28 @@
+import standard
+import advance
 from PySide6.QtWidgets import *
 from PySide6.QtUiTools import *
 from PySide6.QtCore import *
 
 
-class Window(QMainWindow):
 
+class Window(QMainWindow, QWidget):
     def __init__(self):
-        super().__init__()
+        super(Window, self).__init__()
         loader = QUiLoader()
-        self.ui = loader.load('start.ui', None)
+        self.ui = loader.load('project_python_Assignment_16/start.ui', None)
         self.ui.show()
 
-        self.ui.rBtnStd.clicked.connect(self.standard)
-        self.ui.rBtnAdv.clicked.connect(self.advance)
-        self.ui.ExitBtn.clicked.connect(self.btnExin)
-        self.ui.OkBtn.clicked.connect(self.btnExin)
+        self.ui.BtnStd.clicked.connect(self.standard)
+        self.ui.BtnAdv.clicked.connect(self.advance)
+        self.ui.ExitBtn.clicked.connect(self.ui.close)
 
 
     def standard(self):
-        self.loader = QUiLoader()
-        self.ui = self.loader.load('standard.ui', None)
-        self.ui.show()
+        self.addClass = standard.standardClass(self)
 
     def advance(self):
-        self.loader = QUiLoader()
-        self.ui = self.loader.load('advance.ui', None)
-        self.ui.show()
-    
-    def btnExin(self):
-        self.close()
-
-
+        self.addClass = advance.advanceClass(self)
 
 
 app = QApplication([])
